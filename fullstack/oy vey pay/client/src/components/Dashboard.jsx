dc5acc2ba3fb236c052b2c10409f423cdf6eb238
ngalanti <galanti.nehoray@gmail.com>
2025-01-14 13:08:41 +0200

    14/01/25

--- fullstack/oy vey pay/client/src/components/Dashboard.jsx
+++ fullstack/oy vey pay/client/src/components/Dashboard.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
-import { Navbar } from './Navbar'
+import { useAuth } from './AuthProvider'
+
 export const Dashboard = () => {
+  const { user } = useAuth();
   return (
-    <div>Dashboard</div>
-  )
+    <div className="dashboard"> 
+      <header className="dashboard-header">
+        <h1>Hello {user.fullname}</h1>
+      </header>
+
+      <div className="summary">
+        <div className="card income">
+          <h2>Total Incomes</h2>
+          <p>$1000</p>
+        </div>
+      </div>
+
+      <div className="summary">
+        <div className="card expense">
+          <h2>Total Expenses</h2>
+          <p>$500</p>
+        </div>
+      </div>
+
+      <div className="summary">
+        <div className="card balance">
+          <h2>Total Balance</h2>
+          <p>$500</p>
+        </div>
+      </div>
+    </div>
+  );
 }
